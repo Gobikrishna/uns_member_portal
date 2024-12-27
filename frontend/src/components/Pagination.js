2d5fc30e6365376ef639bc36a639b7fb83d72bbe
unsarumugam <unsarumugam2024@gmail.com>
2024-12-27 13:08:56 +0530

    added

---
+++ frontend/src/components/Pagination.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-green text-white">
+    <div className="container text-center">&copy; 2024 UNS. All rights reserved.</div>
+  </footer>
+  )
+}
+
+export default Footer
